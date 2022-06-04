903114cb70fb7ff24b0727386459ae99d25c0a27
Ismael Arce <ismael.arce@elaniin.com>
2022-06-03 22:53:00 -0600

    some changes

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -4,8 +4,8 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 const { QnAMaker } = require('botbuilder-ai');
-const DentistScheduler = require('./dentistscheduler');
-const IntentRecognizer = require("./intentrecognizer")
+// const DentistScheduler = require('./dentistscheduler');
+// const IntentRecognizer = require("./intentrecognizer")
 
 class DentaBot extends ActivityHandler {
     constructor(configuration, qnaOptions) {
@@ -14,7 +14,7 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
+        this.QnAMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions)
        
         // create a DentistScheduler connector
       
@@ -24,7 +24,7 @@ class DentaBot extends ActivityHandler {
         this.onMessage(async (context, next) => {
             // send user input to QnA Maker and collect the response in a variable
             // don't forget to use the 'await' keyword
-          
+            const qnaResults = await this.QnAMaker.getAnswers(context);
             // send user input to IntentRecognizer and collect the response in a variable
             // don't forget 'await'
                      
@@ -37,14 +37,22 @@ class DentaBot extends ActivityHandler {
             //  return;
             // }
             // else {...}
-             
+            if (qnaResults[0]) {
+                console.log(qnaResults[0])
+                await context.sendActivity(`${qnaResults[0].answer}`);
+             }
+             else {
+                 // If no answers were returned from QnA Maker, reply with help.
+                 await context.sendActivity(`I'm not sure I found an answer to your question'
+                 You can ask me questions about electric vehicles like "how can I charge my car?"`);
+              }
             await next();
     });
 
         this.onMembersAdded(async (context, next) => {
         const membersAdded = context.activity.membersAdded;
         //write a custom greeting
-        const welcomeText = '';
+        const welcomeText = 'Hello and welcome!';
         for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
             if (membersAdded[cnt].id !== context.activity.recipient.id) {
                 await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
